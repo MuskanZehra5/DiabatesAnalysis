30d58c1d367ef5f0e203169948f7e45e5d67ea6d
MuskanZehra5 <muskanzehra123@gmail.com>
2025-08-14 20:44:03 +0500

    removed ui product

--- health-risk-ui/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { AssessmentFormComponent } from './features/assessment/assessment-form/assessment-form.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/assess', pathMatch: 'full' },
-  { path: 'assess', component: AssessmentFormComponent },
-  { path: 'patients', loadComponent: () => import('./features/patient/patient-profile/patient-profile.component').then(m => m.PatientProfileComponent) },
-  { path: '**', redirectTo: '/assess' }
-];
